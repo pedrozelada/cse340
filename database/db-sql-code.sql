d745765fd35d425516602bae4cac5eea239c2fce
pedrozelada <przs.gideon@gmail.com>
2024-10-02 20:47:40 -0400

    week2.1

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -247,7 +247,7 @@ UPDATE public.inventory
 SET inv_description = REPLACE(inv_description, 'small interiors','huge interior')
 WHERE inv_id = 10;
 
-
+-- 6 Update Route
 UPDATE public.inventory
 SET inv_image = REPLACE(inv_image, '/images/','/images/vehicles/'),
 	inv_thumbnail = REPLACE(inv_thumbnail, '/images/','/images/vehicles/');
